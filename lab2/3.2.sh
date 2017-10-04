d1d54c6efc0199663e3606aeeda74b3b2aeeacf8
Ruben Vasconcelos <ruben.vasconcelos2@mail.dcu.ie>
2017-10-04 19:35:49 +0100

    Final lab2 solutions

--- lab2/3.2.sh
+++ lab2/3.2.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
 
+# Often you will want to timestamp shell generated files E.g backups and logs
+file_name=`mktemp temp_cronix_$(date "+%Y-%m-%d-%H-%M-%S").XXXXXX` ||
+exit 1
 
+"$@" >$file_name 2>&1
+
+exit_code=$?
+
+[ "$exit_code" -ne "0" ] &&
+cat $file_name
+
+rm $file_name &&
+exit $exit_code
