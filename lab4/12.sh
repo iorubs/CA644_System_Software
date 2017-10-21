63b4411ba1a85eadbd6294ddd72ecfd3c0a25b55
Ruben Vasconcelos <ruben.vasconcelos2@mail.dcu.ie>
2017-10-21 12:00:49 +0100

    Finished Simple questions lab4

--- lab4/12.sh
+++ lab4/12.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
+arg=$(echo "$1" | sed -re '/^-[0-9]+$/! d' -e 's/-//g')
+test -n "$arg" &&
+    cut=$arg
 
+cut=${cut:=10}
+
+sed -n 1,${cut}p
--- lab4/7.sh
+++ lab4/7.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
 
+echo "12 123 45 6" | sed -r 's/(^\s*\S.*$)/& # comment/g'
+echo "   " | sed -r 's/(^\s*\S.*$)/& # comment/g'
