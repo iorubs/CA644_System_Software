141991510ccff3bdbbd318c377e64817884376fc
Ruben Vasconcelos <ruben.vasconcelos2@mail.dcu.ie>
2017-10-23 18:37:21 +0100

    Final lab 4 solutions

--- lab4/17.sh
+++ lab4/17.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-
+while true
+do
+    load=$(uptime | sed -rn 's/.*:\s([0-9]).*/\1/p')
+    test $load -ge 1 &&
+    echo "System overloaded: load average is 1.00 or more."
+    sleep 1
+done
--- lab4/18.sh
+++ lab4/18.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
+seq 10 | sed -ne 'h; n; p; g; p;'
 
+# could also be done with
+# seq 10 | sed -r 'N;s/(.*)\n(.*)/\2\n\1/'
